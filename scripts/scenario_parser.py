41dbb2f2a5b1e2a24bf27a57fbdb2def848dd836
haruyma8940 <yaruo0406@gmail.com>
2022-11-23 21:29:49 +0900

    add scenario_navigation_msgs

--- scripts/scenario_parser.py
+++ scripts/scenario_parser.py
@@ -7,23 +7,30 @@ import rospy
 import rosparam
 from std_msgs.msg import String
 #from jsk_rviz_plugins.msg import OverlayText
-from  scenario_navigation.srv import Scenario
+from scenario_navigation.srv import Scenario
 import MeCab
+from jsk_rviz_plugins.msg import OverlayText
 
 class ScenarioParser:
     def __init__(self):
         self.hz = 1
         self.loop_rate = rospy.Rate(self.hz)
         self.scenario_service_proxy_ = rospy.ServiceProxy('scenario', Scenario)
-       
+        # self.scenario_text_pub = rospy.Publisher("text",OverlayText,1)
         self.ACTIONS = ["直進", "前進", "右折", "左折", "向く", "停止", "曲がる"]
         self.DIRECTIONS = ["前", "前方", "右", "右手", "左", "左手", "後ろ", "後方"]
-        self.TYPE = ["一本道", "三叉路", "行き止まり", "十字路", "突き当り", "曲がり角", "角", "通路", "交差点"]
+        self.TYPE = ["一本道", "三叉路", "行き止まり", "十字路",
+                     "突き当り", "曲がり角", "角", "通路", "交差点"]
         self.ORDERS = ["つ", "番", "次"]
 
-        self.ACTIONS_ENG = ["go_straight", "go_straight", "turn_right", "turn_left", "turn_", "stop", "turn_"]
-        self.DIRECTIONS_ENG = ["front", "front", "right", "right", "left", "left", "back", "back"]
-        self.TYPE_ENG = ["straight_road", "3_way", "dead_end", "crossroads", "end", "corridor", "corridor", "corridor", "corridor"]
+        self.ACTIONS_ENG = ["go_straight", "go_straight",
+                            "turn_right", "turn_left", "turn_", "stop", "turn_"]
+        self.DIRECTIONS_ENG = ["front", "front", "right",
+                               "right", "left", "left", "back", "back"]
+        # self.TYPE_ENG = ["straight_road", "3_way", "dead_end", "crossroads",
+        #                  "end", "corridor", "corridor", "corridor", "corridor"]
+        self.TYPE_ENG = ["straight_road", "3_way", "dead_end", "crossroads",
+                         "corridor", "corridor", "corridor", "corridor", "corridor"]
 
         self.action_ = []
 #        self.distance_ = []
@@ -33,11 +40,12 @@ class ScenarioParser:
         self.condition_ = []
         self.mecab = MeCab.Tagger('-Owakati -d /usr/lib/x86_64-linux-gnu/mecab/dic/mecab-ipadic-neologd')
         #self.mecab = MeCab.Tagger('-Owakati -d /usr/share/mecab/dic/ipadic')
+        # self.mecab = MeCab.Tagger('-Owakati')
 
 # complement direction to turn if scenario-action is "turn_"
     def complement_scenario_action_turn_xxx(self, direction, action):
-        if(action == "turn_"):
-            if(direction == "left" or direction == "right" or direction == "back"):
+        if (action == "turn_"):
+            if (direction == "left" or direction == "right" or direction == "back"):
                 result = action + direction
         else:
             result = action
@@ -47,34 +55,35 @@ class ScenarioParser:
 # Translate each features(for example, action and direction) of scenario from Japanese to English
 # Not translate a word about "order" in scenario because order is integer
     def translation_from_ja_to_en(self, types, direction, action):
-        if(types in self.TYPE):
+        if (types in self.TYPE):
             type_index = self.TYPE.index(types)
             return_type = self.TYPE_ENG[type_index]
         else:
             return_type = types
 
-        if(direction in self.DIRECTIONS):
+        if (direction in self.DIRECTIONS):
             direction_index = self.DIRECTIONS.index(direction)
             return_direction = self.DIRECTIONS_ENG[direction_index]
         else:
             return_direction = direction
 
-        if(action in self.ACTIONS):
+        if (action in self.ACTIONS):
             action_index = self.ACTIONS.index(action)
             return_action = self.ACTIONS_ENG[action_index]
         else:
             return_action = action
-        return_action = self.complement_scenario_action_turn_xxx(return_direction, return_action)
+        return_action = self.complement_scenario_action_turn_xxx(
+            return_direction, return_action)
 
         return return_type, return_direction, return_action
 
 # get a word related to "distance"
     def get_distance(self, sentence):
         try:
-            tmp = [i for i, word in enumerate(sentence) if("m" in word)]
-            if(len(tmp) != 0):
+            tmp = [i for i, word in enumerate(sentence) if ("m" in word)]
+            if (len(tmp) != 0):
                 i = tmp[0]
-                if("m" in sentence[i]):
+                if ("m" in sentence[i]):
                     tmp = sentence[i]
                     distance = int(tmp.rstrip("m"))
                     return distance
@@ -89,13 +98,14 @@ class ScenarioParser:
 # get a word related to "order"
     def get_order(self, sentence):
         try:
-            tmp = [i for i, word in enumerate(sentence) for order in self.ORDERS if(order in word)]
-            if(len(tmp) != 0):
+            tmp = [i for i, word in enumerate(
+                sentence) for order in self.ORDERS if (order in word)]
+            if (len(tmp) != 0):
                 i = tmp[0]
-                if(sentence[i] == "次"):
+                if (sentence[i] == "次"):
                     order = 1
                     return int(order)
-                elif(sentence[i+1] == "目"):
+                elif (sentence[i+1] == "目"):
                     order = sentence[i].rstrip("つ")
                     order = order.rstrip("番")
                     order = int(order)
@@ -111,9 +121,9 @@ class ScenarioParser:
 # get a word related to "direction"
     def get_direction(self, sentence):
         try:
-            tmp = [i for i, word in enumerate(sentence) for direction in self.DIRECTIONS if((direction in word) and \
-                                                                                              (("右折" not in word) and ("左折" not in word)))]
-            if(len(tmp) != 0):
+            tmp = [i for i, word in enumerate(sentence) for direction in self.DIRECTIONS if ((direction in word) and
+                                                                                             (("右折" not in word) and ("左折" not in word)))]
+            if (len(tmp) != 0):
                 i = tmp[0]
                 direction = sentence[i]
                 return direction
@@ -127,10 +137,12 @@ class ScenarioParser:
 
 
 # get a word related to "type"
+
     def get_type(self, sentence):
         try:
-            tmp = [i for i, word in enumerate(sentence) for road_type in self.TYPE if(road_type in word)]
-            if(len(tmp) != 0):
+            tmp = [i for i, word in enumerate(
+                sentence) for road_type in self.TYPE if (road_type in word)]
+            if (len(tmp) != 0):
                 i = tmp[0]
                 road_type = sentence[i]
                 return road_type
@@ -145,8 +157,9 @@ class ScenarioParser:
 # get a word related to "action"
     def get_action(self, sentence):
         try:
-            tmp = [i for i, word in enumerate(sentence) for action in self.ACTIONS if(action in word)]
-            if(len(tmp) != 0):
+            tmp = [i for i, word in enumerate(
+                sentence) for action in self.ACTIONS if (action in word)]
+            if (len(tmp) != 0):
                 i = tmp[0]
                 action = sentence[i]
                 return action
@@ -161,11 +174,11 @@ class ScenarioParser:
 # get a word related to condition
     def get_condition(self, sentence):
         try:
-            tmp = [i for i, word in enumerate(sentence) if("まで" in word)]
-            if(len(tmp) != 0):
+            tmp = [i for i, word in enumerate(sentence) if ("まで" in word)]
+            if (len(tmp) != 0):
                 i = tmp[0]
                 condition = sentence[i]
-                if("見える" in sentence[i-1]):
+                if ("見える" in sentence[i-1]):
                     condition = "見えるまで"
 
                 return condition
@@ -205,17 +218,19 @@ class ScenarioParser:
 # send conditions and action
     def send_scenarios(self, i):
 
-    # debug
-#        print("type is ", self.type_[i])
-#        print("order is ", self.order_[i])
-#        print("direction is ", self.direction_[i])
-#        print("action is ", self.action_[i])
+        # debug
+        #        print("type is ", self.type_[i])
+        #        print("order is ", self.order_[i])
+        #        print("direction is ", self.direction_[i])
+        #        print("action is ", self.action_[i])
 
         scenario_order = self.order_[i]
-        scenario_type, scenario_direction, scenario_action = self.translation_from_ja_to_en(self.type_[i], self.direction_[i], self.action_[i])
+        scenario_type, scenario_direction, scenario_action = self.translation_from_ja_to_en(
+            self.type_[i], self.direction_[i], self.action_[i])
 
         try:
-            self.scenario_service_proxy_(scenario_type, scenario_order, scenario_direction, scenario_action)
+            self.scenario_service_proxy_(
+                scenario_type, scenario_order, scenario_direction, scenario_action)
         except rospy.ServiceException as e:
             print("Service call failed: ", e)
 
---
+++ src/cmd_dir_executor_old.cpp
@@ -0,0 +1,355 @@
+#include "ros/ros.h"
+#include "sensor_msgs/LaserScan.h"
+#include "std_msgs/Bool.h"
+#include "std_msgs/String.h"
+#include "std_msgs/Float32.h"
+#include <std_msgs/Int8MultiArray.h>
+#include "geometry_msgs/Twist.h"
+#include "scenario_navigation/Scenario.h"
+#include "scenario_navigation/PassageType.h"
+#include <unistd.h>
+#include <cmath>
+#include <vector>
+
+#include <iostream>
+
+class cmdVelController {
+     public:
+        cmdVelController();
+        int SCENARIO_MAX = 10;
+        void getRosParam(void);
+        // bool compareScenarioAndPassageType(const scenario_navigation::PassageType::ConstPtr& passage_type);
+        // void loadNextScenario(void);//
+        // void updateLastNode(bool center_flg, bool back_flg, bool left_flg, bool right_flg);
+
+        // bool compareLastNodeAndCurrentNode(const scenario_navigation::PassageType::ConstPtr& passage_type);
+        // void turnFinishFlgCallback(const std_msgs::Bool::ConstPtr& turn_finish_flg); //
+        // void passageTypeCallback(const scenario_navigation::PassageType::ConstPtr& passage_type);
+
+        // <intersection>
+        bool compareScenarioAndPassageType(const std_msgs::String::ConstPtr& intersection_name);
+        void loadNextScenario(void);//
+        void updateLastNode(const std_msgs::String::ConstPtr& intersection_n);
+        bool compareLastNodeAndCurrentNode(const std_msgs::String::ConstPtr& intersection_name);
+        void turnFinishFlgCallback(const std_msgs::Bool::ConstPtr& turn_finish_flg);
+        void passageTypeCallback(const std_msgs::String::ConstPtr& intersection_name);
+        
+        void stopCallback(const std_msgs::Bool::ConstPtr& stop); //
+        bool scenarioCallback(scenario_navigation::Scenario::Request& scenario,
+                              scenario_navigation::Scenario::Response& res);
+     private:
+        ros::NodeHandle node_;
+        /*
+        ros::Publisher stop_pub_;
+        ros::Publisher rotate_rad_pub_;
+        ros::Publisher cmd_data_pub;
+        ros::Subscriber passage_type_sub_;
+        ros::Subscriber stop_sub_;
+        ros::Subscriber turn_finish_flg_sub_;
+        ros::ServiceServer scenario_server_;
+        */
+
+        // <intersection>
+        ros::Publisher stop_pub_;
+        ros::Publisher cmd_data_pub;
+        ros::Subscriber intersection_sub_;
+        ros::Subscriber passage_type_sub_;
+        ros::Subscriber stop_sub_;
+        ros::Subscriber turn_finish_flg_sub_;
+        ros::ServiceServer scenario_server_;
+
+
+        std::list<std::string> target_type_;
+        std::list<std::int16_t> target_order_;
+        std::list<std::string> target_direction_;
+        std::list<std::string> target_action_;
+
+        std::list<std::string>::iterator target_type_itr_begin_;
+        std_msgs::Int8MultiArray cmd_data;
+        std::list<std::int16_t>::iterator target_order_itr_begin_;
+        std::list<std::string>::iterator target_direction_itr_begin_;
+        std::list<std::string>::iterator target_action_itr_begin_;
+
+        std::string action;
+        int scenario_num_ = 0;
+        int scenario_progress_cnt_ = 0;
+        int scenario_order_cnt_ = 1;
+        int reach_target_type_cnt_ = 0;
+        int reach_different_type_cnt_ = 0;
+        int reach_target_type_cnt_margin_ = 6;
+        int reach_different_type_cnt_margin_ = 6;
+        float rotate_rad_ = 0.0;
+        bool turn_flg_ = false;
+        bool change_node_flg_ = false;
+        bool satisfy_conditions_flg_ = false;
+        bool stop_flg_ = true;
+        bool request_update_last_node_flg = true;
+        std_msgs::Float32 rotate_rad_for_pub_;
+        std_msgs::String last_node_;
+        int str_list[3] = {1,0,0};
+        int left_list[3] = {0,1,0};
+        int right_list[3] = {0,0,1};
+};
+
+cmdVelController::cmdVelController(){
+    stop_pub_ = node_.advertise<std_msgs::Bool>("stop", 1, false);
+    // rotate_rad_pub_ = node_.advertise<std_msgs::Float32>("rotate_rad", 1, false);
+
+    // turn_finish_flg_sub_ = node_.subscribe<std_msgs::Bool> ("turn_finish_flg", 1, &cmdVelController::turnFinishFlgCallback, this);
+   
+    passage_type_sub_ = node_.subscribe<std_msgs::String> ("passage_type", 1, &cmdVelController::passageTypeCallback, this); //intersection name
+
+    stop_sub_ = node_.subscribe<std_msgs::Bool> ("stop", 1, &cmdVelController::stopCallback, this);
+    scenario_server_ = node_.advertiseService("scenario", &cmdVelController::scenarioCallback, this);
+    cmd_data_pub = node_.advertise<std_msgs::Int8MultiArray >("cmd_dir", 1);
+    cmd_data.data.resize(3);
+
+    // updateLastNode();
+    getRosParam();
+}
+
+void cmdVelController::getRosParam(void){
+    SCENARIO_MAX = 10;
+    node_.getParam("scenario_executor/scenario_max", SCENARIO_MAX);
+}
+//<intersection>
+bool cmdVelController::compareScenarioAndPassageType(const std_msgs::String::ConstPtr& passage_type){
+    std::string target_type = *std::next(target_type_itr_begin_, scenario_progress_cnt_);
+    std::string target_direction = *std::next(target_direction_itr_begin_, scenario_progress_cnt_);
+
+    //check "straight_road"
+    if(target_type == "straight_road"){
+        if(passage_type->data == last_node_.data){
+            return true;
+        }
+    }
+
+    // check 3_way_left and 3_way_right when 3_way is designated by scenario
+    if(target_type == "3_way"){
+        if(passage_type->data == last_node_.data){
+            return true;
+        }
+        }
+
+    // check "end"(= 突き当り)
+    if(target_type == "end"){
+    // dead_end
+         if(passage_type->data == last_node_.data){
+            return true;
+        }
+       
+    }
+
+    // check "corridor"(ex, 交差点， 通路)
+    if(target_type == "corridor"){
+        if(target_direction == "left"){
+            if(passage_type->data == last_node_.data){
+                return true;
+            }
+        }
+        else if(target_direction == "right"){
+            if(passage_type->data==last_node_.data){
+                return true;
+            }
+        }
+    // if target_direction is not designated, do below
+        // else{
+        //     if(passage_type->left_flg || passage_type->right_flg){
+        //         return true;
+        //     }
+        }
+    // }
+    return false;
+}
+
+void cmdVelController::loadNextScenario(void){
+    // std::string action = *std::next(target_action_itr_begin_, scenario_progress_cnt_);
+    std::string action = *std::next(target_action_itr_begin_, scenario_progress_cnt_);
+
+// stop robot
+    stop_flg_ = true;
+
+    if(action == "stop"){
+        ROS_INFO("Robot gets a goal");
+        std_msgs::Bool stop_flg_for_pub;
+        stop_flg_for_pub.data = stop_flg_;
+        // stop_pub_.publish(stop_flg_for_pub);
+    }
+    // else if(action == "go_straight")
+    //     std::copy(std::begin(str_list),std::end(str_list),std::begin(cmd_data.data));
+    else{
+        ROS_INFO("Execute next action(%s)", action.c_str());//string <=> char
+        stop_flg_ = false;
+        change_node_flg_ = false;
+        
+        if(action.find("turn") != std::string::npos){
+            //turn_flg_ = true;
+
+            //if(action.find("left")){
+            if(action == "turn_left"){
+                std::copy(std::begin(left_list),std::end(left_list),std::begin(cmd_data.data));
+            
+            }
+            //else if(action.find("right")){
+            else if(action == "turn_right"){
+                std::copy(std::begin(right_list),std::end(right_list),std::begin(cmd_data.data));
+                
+            }
+        }
+        else{
+            std::copy(std::begin(str_list),std::end(str_list),std::begin(cmd_data.data));
+        }
+        cmd_data_pub.publish(cmd_data);
+    }
+}
+
+// <intersection>
+void cmdVelController::updateLastNode(const std_msgs::String::ConstPtr& intersection_n){
+    ROS_INFO("update last node");
+    ROS_INFO("last node is (%s)",intersection_n);
+   last_node_.data = intersection_n->data;
+}
+
+bool cmdVelController::compareLastNodeAndCurrentNode(const std_msgs::String::ConstPtr& intersection_name){
+    if(last_node_.data == intersection_name->data){
+            return true;
+        }
+    else{
+        return false;
+    }
+ }
+
+void cmdVelController::passageTypeCallback(const std_msgs::String::ConstPtr& passage_type){
+//std::copy(std::begin(left_list),std::end(left_list),std::begin(cmd_data.data));
+    if(! stop_flg_){
+        if(request_update_last_node_flg){
+            updateLastNode(passage_type);
+            request_update_last_node_flg = false;
+        }
+        if(! turn_flg_){
+            if(change_node_flg_){
+                //cmd_data_pub.publish(cmd_data);
+                satisfy_conditions_flg_ = compareScenarioAndPassageType(passage_type);
+                if(satisfy_conditions_flg_){
+                    ROS_INFO("find target node");
+                    reach_target_type_cnt_++;
+                    if(reach_target_type_cnt_margin_ <= reach_target_type_cnt_){
+                        reach_target_type_cnt_ = 0;
+                        scenario_order_cnt_++;
+                        change_node_flg_ = false;
+                        updateLastNode(passage_type);
+                        int order = *std::next(target_order_itr_begin_, scenario_progress_cnt_);
+                        if(order <= scenario_order_cnt_){
+                            ROS_INFO("Robot reaches target_node!!");
+                            scenario_order_cnt_ = 0;
+                            scenario_progress_cnt_++;
+                            loadNextScenario();
+                        }
+                    }
+                }
+                else{
+                    reach_target_type_cnt_ = 0;
+                    cmd_data_pub.publish(cmd_data);
+                }
+                if (action == "turn_left" || action == "turn_right")
+                    {
+                        ROS_INFO("finish turn");
+                        turn_flg_ = false;
+                        scenario_progress_cnt_++;
+                        loadNextScenario();
+                        request_update_last_node_flg = true;
+                        //change_node_flg_ = false;
+                    }
+            }
+            else{
+                if(! compareLastNodeAndCurrentNode(passage_type)){
+                    reach_different_type_cnt_++;
+                    if (action == "turn_left" || action == "turn_right")
+                    {
+                        ROS_INFO("finish turn");
+                        turn_flg_ = false;
+                        scenario_progress_cnt_++;
+                        loadNextScenario();
+                        request_update_last_node_flg = true;
+                        //change_node_flg_ = false;
+                    }
+                    
+                    if(reach_different_type_cnt_margin_ <= reach_different_type_cnt_){
+                        reach_different_type_cnt_ = 0;
+                        updateLastNode(passage_type);
+                        change_node_flg_ = true;
+                    }
+                }
+                else{
+                    reach_different_type_cnt_ = 0;
+                }
+            }
+        }
+    }
+    //cmd_data_pub.publish(cmd_data);
+}
+void cmdVelController::turnFinishFlgCallback(const std_msgs::Bool::ConstPtr& turn_finish_flg){
+    ROS_INFO("finish turn");
+    turn_flg_ = false;
+    scenario_progress_cnt_++;
+    loadNextScenario();
+    request_update_last_node_flg = true;
+    change_node_flg_ = false;
+}
+
+void cmdVelController::stopCallback(const std_msgs::Bool::ConstPtr& stop_flg){
+    stop_flg_ = stop_flg->data;
+}
+
+bool cmdVelController::scenarioCallback(scenario_navigation::Scenario::Request& scenario,
+                                        scenario_navigation::Scenario::Response& res){
+    scenario_num_++;
+    target_type_.push_back(scenario.type);
+    target_order_.push_back(scenario.order);
+    target_direction_.push_back(scenario.direction);
+    target_action_.push_back(scenario.action);
+
+    std::string last_action = *std::next(target_action_.begin(), scenario_num_ - 1);//先頭を取得  シナリオナム方向へすすめる
+// check whether scenario is loaded
+    if(last_action == "stop"){
+        ROS_INFO("Completed loading scenario");
+
+        target_type_itr_begin_ = target_type_.begin();
+        target_order_itr_begin_ = target_order_.begin();
+        target_direction_itr_begin_ = target_direction_.begin();
+        target_action_itr_begin_ = target_action_.begin();
+
+        stop_flg_ = false;
+        std_msgs::Bool stop_flg_for_pub;
+        stop_flg_for_pub.data = stop_flg_;
+        stop_pub_.publish(stop_flg_for_pub);
+        loadNextScenario();
+    }
+    else{
+        stop_flg_ = true;
+        std_msgs::Bool stop_flg_for_pub;
+        stop_flg_for_pub.data = stop_flg_;
+        stop_pub_.publish(stop_flg_for_pub);
+    }
+
+
+//  debug
+    std::cout << "####################################" << std::endl;
+    std::cout << "type is " << scenario.type << std::endl;
+    std::cout << "order is "  << std::hex << scenario.order << std::endl;
+    std::cout << "direction is " << scenario.direction << std::endl;
+    std::cout << "action is " << scenario.action << std::endl;
+    std::cout << "####################################" << std::endl;
+
+    return true;
+}
+
+int main(int argc, char** argv){
+    ros::init(argc, argv, "scenario_executor");
+    cmdVelController cmd_vel_controller;
+    while(ros::ok()){
+        ros::spin();
+    }
+
+    return 0;
+}
